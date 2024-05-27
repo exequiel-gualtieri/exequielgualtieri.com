e19021c7a6024dddbf34d5dfc81e20572d70db3b
exequiel-gualtieri <exequielgualtieri.dev@gmail.com>
2024-05-27 19:27:56 +0200

    FontAwesome for Angular v17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NeonlightMouseDirective as MouseEffect } from '../../submodules/@rhythmCore/directives/neon-light-mouse/neonlight-mouse.directive';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MouseEffect, FontAwesomeModule]
 })
 export class AppComponent {
   title = 'exequielgualtieri.com';
